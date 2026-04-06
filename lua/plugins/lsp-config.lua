0c9a639487128ce1a852add35270cb46594f0e0f
walter <wtsimaruk@gmail.com>
2026-04-05 21:49:21 -0400

    LSP basics

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,74 @@
+local mason_conf = {
+    "mason-org/mason.nvim",
+    config = function() 
+      require("mason").setup()
+    end
+}
+
+local mason_lsp_conf = {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        "mason-org/mason.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "rust_analyzer", "ts_ls","jdtls"},
+      }) 
+    end
+}
+
+local mason_nvim_dap_conf = {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function() 
+      require("mason-nvim-dap").setup({
+        ensure_installed = {"java-debug-adapter", "java-test"}
+      })
+    end
+}
+
+local nvim_jdtls_conf = {
+  "mfussenegger/nvim-jdtls",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+  } 
+}
+
+local lsp_conf = {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconf = vim.lsp.config
+    local lsp = vim.lsp
+
+
+    lsp.enable('lua_ls')
+
+    lsp.enable('ts_ls')
+
+    vim.keymap.set("n", "<leader>co", vim.lsp.buf.hover, { desc = "[C]ode Hover D[o]cumentation" })
+    vim.keymap.set("n", "<leader>cg", vim.lsp.buf.definition, { desc = "[C]ode [G]oto Defintion" })
+    vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Show [R]eferences" })
+    vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Show [I]mplementations" })
+    vim.keymap.set({"n","v"}, "<leader>ca", vim.lsp.buf.code_action, { desc = "Show [C]ode [A]ctions" })
+    vim.keymap.set("n", "<leader>cE", vim.lsp.buf.rename, { desc = "[C]ode R[e]name"})
+    vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode [D]eclaration"})
+
+
+
+  end
+}
+
+
+
+
+local parent = {
+  mason_conf, mason_lsp_conf, mason_nvim_dap_conf, nvim_jdtls_conf,lsp_conf
+}
+
+
+
+return parent
